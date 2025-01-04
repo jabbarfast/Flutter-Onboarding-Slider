5bd0e831682a67abd0e4a69dead7c221f79ae5fe
Muhammad Jabbar <jabbar.fastnu@gmail.com>
2025-01-04 11:34:43 +0500

    Reduced the switching time between Splash screen and Onboarding screen.

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
     // TODO: implement initState
     super.initState();
     Future.delayed(
-      const Duration(seconds: 10),
+      const Duration(seconds: 2),
       () {
         _showOnboardingScreens();
       },
